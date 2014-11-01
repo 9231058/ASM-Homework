2af737db35c8cd4066a9a360faf2c8c21cd71d69
Parham Alvani <parham.alvani@gmail.com>
2014-11-01 19:11:58 +0330

    Homework-5, problem 1 improved.

--- Homework-5/1/1.asm
+++ Homework-5/1/1.asm
@@ -1,17 +1,14 @@
 section .data
-	n: dq 100000
-	format: db "Sum is %d", 10, 0
+	n: dq 20
+section .bss
+	write_byte: resb 1	
 section .text
-	extern printf
-	global main
-main:
+	global _start
+_start:
 	mov rax, [n]
 	call sum
 
-	mov rdi, format
-	mov rsi, rax
-	mov rax, 0
-	call printf
+	call print
 	
 	mov eax, 1
 	mov ebx, 0
@@ -32,4 +29,46 @@ sum:
 sum_ret_0:
 	mov rax, 0
 sum_ret:
+	ret
+print:
+	mov r8, 0
+digit:
+	inc r8
+
+	mov rcx, 10
+	mov rdx, 0
+	div rcx
+	
+	add rdx, 30H
+	push rdx
+	
+	test rax, rax
+	jnz digit
+write:
+	dec r8
+	
+	pop rdx
+	mov byte [write_byte], dl
+
+	push r8
+
+	mov rax, 4
+	mov rbx, 1
+	mov rcx, write_byte
+	mov rdx, 1
+	int 80H
+
+	pop r8
+
+	test r8, r8
+	jnz write
+
+	mov byte [write_byte], 10
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, write_byte
+	mov edx, 1
+	int 80H
+
 	ret
